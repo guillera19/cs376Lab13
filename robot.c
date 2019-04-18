aa04e5331acc81c7f874e46f188a896031dd209d
simonk21 <simonk21@egr3.campus.up.edu>
2019-04-18 13:11:41 -0700

    Animated robot 3 times.

--- robot.c
+++ robot.c
@@ -34,14 +34,16 @@ void robotPrintAscii(void)
   int i;
   char* blank = "                              ";
   blank = blank +30;
+  printf("\n\n" "Uwr: Unidentified Wheeled Robot\n"); 
+  for(i = 0; i < 3; i++){
   printf("\n\n"
-	 "UWR: Unidentified Wheeled Robot\n"
 	 "%s    i_i    \n"
 	 "%s   [*_*]   \n"
 	 "%s  /|___|\\ \n"
 	 "%s   d   b   \n",blank, blank, blank, blank);
 
   blank = blank - 10;
+  }
 }
 
 /**
